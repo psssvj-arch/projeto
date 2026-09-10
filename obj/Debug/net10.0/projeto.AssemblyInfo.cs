744a509655273cfd296426ef38ae9275742fcb7f
psssvj-arch <psssvj@gmail.com>
2026-09-09 21:51:05 -0300

    .

--- obj/Debug/net10.0/projeto.AssemblyInfo.cs
+++ obj/Debug/net10.0/projeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+028d01ed30a50eb43fee6afe2d4669ea0977e094")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3283308b2141038fe40641f55715d0ce773174ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("projeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
